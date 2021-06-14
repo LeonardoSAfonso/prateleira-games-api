e499f1548a43c93333cb60aff94ebb7d8bbf98a3
Leonardo <leonardo.s_afonso@hotmail.com>
2021-06-14 17:57:25 -0300

    feat: - Add version node

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ dotenv.config({
 });
 
 module.exports = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USER,
