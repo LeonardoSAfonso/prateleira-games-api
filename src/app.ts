47f39d474090ea958fd323cdac86eba637aea065
Leonardo <leonardo.s_afonso@hotmail.com>
2021-06-14 18:50:40 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors';
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
